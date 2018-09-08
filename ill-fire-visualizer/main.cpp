2d1e9f1609243d36de4a300e96d58348785ca9ba
Max Cruz <cruz.max@husky.neu.edu>
2018-09-08 15:04:41 -0400

    Initial Commit

---
+++ ill-fire-visualizer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ill-fire-visualizer
+//
+//  Created by Max Cruz on 9/8/18.
+//  Copyright © 2018 Max Cruz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
