ac33dd866e78f4702daba1ed9ba1707e5a83d889
Max Cruz <cruz.max@husky.neu.edu>
2018-09-08 16:01:10 -0400

    adding current local project

--- ill-fire-visualizer/main.cpp
+++ ill-fire-visualizer/main.cpp
@@ -1,15 +1,54 @@
-//
-//  main.cpp
-//  ill-fire-visualizer
-//
-//  Created by Max Cruz on 9/8/18.
-//  Copyright © 2018 Max Cruz. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+const GLint WIDTH = 800, HEIGHT = 800;
+
+int main()
+{
+    glfwInit();
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "ILL FIRE", nullptr, nullptr);
+    
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+    
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        
+        glfwTerminate();
+        
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    
+    glewExperimental = GL_TRUE;
+    
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        
+        return -1;
+    }
+    
+    glViewport(0, 0, screenWidth, screenHeight);
+    
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+    
+    glfwTerminate();
+    
     return 0;
 }
