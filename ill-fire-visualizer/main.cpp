20ef7d08593102b4ac9f7d845a85b5af42061fac
Max Cruz <cruz.max@husky.neu.edu>
2018-09-09 17:12:54 -0400

    added input handling; prints info regarding input track

--- ill-fire-visualizer/main.cpp
+++ ill-fire-visualizer/main.cpp
@@ -6,8 +6,35 @@
 #include <GLFW/glfw3.h>
 const GLint WIDTH = 800, HEIGHT = 800;
 
-int main()
+#include <sndfile.hh>
+
+int main(int argc, const char* argv[])
 {
+    /* handling of input file */
+    if (argc != 2)
+    {
+        std::cout << "Usage: ill-fire-visualizer input_file" << std::endl;
+    }
+    
+    const char* input_file = argv[1];
+    SNDFILE *track_info;
+    SF_INFO sf_info;
+    memset(&sf_info, 0, sizeof(SF_INFO));
+
+    if (! (track_info = sf_open(input_file, SFM_READ, &sf_info)))
+    {
+        std::cout << "Unable to open input file" << std::endl;
+        
+        return -1;
+    }
+    
+    std::cout << "channels: " << sf_info.channels << std::endl;
+    std::cout << "format: " << sf_info.format << std::endl;
+    std::cout << "frames: " << sf_info.frames << std::endl;
+    std::cout << "sample rate: " << sf_info.samplerate << std::endl;
+    std::cout << "sections: " << sf_info.sections << std::endl;
+    
+    /* window initialization */
     glfwInit();
     
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -43,12 +70,19 @@ int main()
     
     glViewport(0, 0, screenWidth, screenHeight);
     
+    /* main rendering loop */
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
         glfwSwapBuffers(window);
     }
     
     glfwTerminate();
+
+    /* clean-up */
+    if (sf_close(track_info) != 0)
+    {
+        std::cout << "Unable to close input file" << std::endl;
+    }
     
     return 0;
 }
